abdca564bc86c09926e26f8f5068b2783a94b8a2
Luis Otávio Lourenço de Souza <luisotavioldesouza1@gmail.com>
2020-08-19 20:42:50 -0300

    atualização model de usuarios e esboço estrutura do backend

---
+++ backend/estruturadb.md
@@ -0,0 +1,22 @@
+## RestAPI
+### Users
+    nome,
+    username,
+    password,
+    email,
+    datanascimento,
+    datacriacao,
+    amigos,
+    settings
+
+### Groups
+    nome,
+    participantes, <- flag de admin
+    datacriacao,
+    criadopor,
+    descricao
+
+## Websocket
+    Responsável por fazer a comunicação client->server server->client
+### npmjs.com/package/ws
+### https://www.js-tutorials.com/nodejs-tutorial/simple-websocket-example-with-nodejs/
--- backend/models/User.js
+++ backend/models/User.js
@@ -15,10 +15,26 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    email:{
+        type: String,
+        required: true
+    },
+    birthday:{
+        type: Date,
+        required: true
+    },
     creationdate: {
         type: Date,
         default: Date.now
     },
+    friends:{
+        type: Array,
+        required: false
+    },
+    settings:{
+        type: Array,
+        required: false
+    }
 });
 
 
